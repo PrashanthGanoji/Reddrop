87d9830b42e1efc92586a5351e082b203d4a0418
ravikiran4 <prashanth.ganoji@gmail.com>
2018-09-14 20:34:01 +0530

    fixed routing issues

--- reddrop/settings.py
+++ reddrop/settings.py
@@ -156,6 +156,6 @@ STATICFILES_DIRS = [
     STATIC_DIR,
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'

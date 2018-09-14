b6bed2233ea7f77f11077e2c0c4c6ff90a56afa2
ravikiran4 <prashanth.ganoji@gmail.com>
2018-09-14 19:15:29 +0530

    fixed config issues

--- frontend/config/paths.js
+++ frontend/config/paths.js
@@ -42,7 +42,7 @@ function getServedPath(appPackageJson) {
 module.exports = {
   dotenv: resolveApp('.env'),
 //  appBuild: resolveApp('build'), //local
-  appBuild: resolveApp('../assets/bundles/'),
+  appBuild: resolveApp('../static/bundles/'),
   appPublic: resolveApp('public'),
   appHtml: resolveApp('public/index.html'),
   appIndexJs: resolveApp('src/index.js'),

1dd3e21ce82fdb7f55237ec739b727d3468cd143
ravikiran4 <prashanth.ganoji@gmail.com>
2018-09-14 21:00:09 +0530

    fixed routing issues

--- frontend/src/resources/links.js
+++ frontend/src/resources/links.js
@@ -1,3 +1,3 @@
-// export const ROOT_URL = 'https://reddrop-donors.herokuapp.com'
+export const ROOT_URL = 'https://reddrop-donors.herokuapp.com'
 
-export const ROOT_URL = 'http://127.0.0.1:8000'
+// export const ROOT_URL = 'http://127.0.0.1:8000'
